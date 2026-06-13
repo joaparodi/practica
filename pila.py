6f55c3228d84daccc116c9083a7e42abf001a504
joaquin <joaquinparodi79@gmail.com>
2026-06-13 01:06:55 -0300

    add functionality to reverse a word using stack

--- pila.py
+++ pila.py
@@ -13,10 +13,36 @@ for i in range(10):
 #conta = 0
 
 #while pila.size()> 0 :
-    x =  pila.pop()
-    if x == num :
-        conta  += 1
+#    x =  pila.pop()
+#    if x == num :
+#        conta  += 1
 
 #print("la cantidad de ocurrencias encontradas son:")
 #print(conta)
     
+#6. Leer una palabra y visualizarla en forma inversa.
+
+pila = Stack()
+
+palabra = input("palabra a invertir:")
+
+for letra in palabra:
+    pila.push(letra)
+
+pila.show()
+
+def invertir(pila):
+    aux = Stack()
+    while pila.size () > 0 :
+        aux.push(pila.pop())
+    
+    print("pila invertida")
+    aux.show()
+    
+    while aux.size () > 0 :
+        pila.push(aux.pop())
+
+invertir(pila)
+
+
+
