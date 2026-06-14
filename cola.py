3136874738002173a3f61b80576e84868907dddb
joaquin <joaquinparodi79@gmail.com>
2026-06-14 12:39:53 -0300

    feat: add functionality to create a character queue and remove vowels

--- cola.py
+++ cola.py
@@ -1,1 +1,34 @@
+from queue import Queue
+from random import randint
+
+
+
+
+
 #1. Eliminar de una cola de caracteres todas las vocales que aparecen.
+
+cola = Queue()
+
+
+vocales =['a','e','i','o','u']
+
+for i in range(10):
+    cola.arrive(chr(randint(97,122)))
+
+print("cola con vocales:")
+cola.show()
+
+def eleminar_v(cola : Queue,vocales : tuple):
+    colaaux = Queue()
+    while cola.size() > 0 :
+        x = cola.attention()
+        if x not in vocales:
+            colaaux.arrive(x)
+       
+    while colaaux.size() > 0:
+        cola.arrive(colaaux.attention())
+
+
+print("cola sin vocales:")    
+eleminar_v(cola,vocales)
+cola.show()
