a520831cacbd721b85f17561d39169d767f359ed
joaquin <joaquinparodi79@gmail.com>
2026-06-26 01:17:05 -0300

    feat: remove temporary code for updating superhero's comic house

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-thor = lista.search( "Thor",'name')
-lista[thor].house = "Marvel"
-if thor is not None:
-    print(f"la nueva casa de thor es:{lista[thor].house}")
-else:
-    print("no se encontro thor")
