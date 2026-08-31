1b0de51619f33dc64e8af39bff5ae9d40e122d22
joaquin <joaquinparodi79@gmail.com>
2026-08-30 23:59:12 -0300

    refactor: remove temporary code for deleting nodes from the tree

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-# c. eliminar tres valores del árbol;
-
-print("eliminar tres valores del árbol")
-
-print('eliminar', arbol.delete_node(20))
-arbol.inorden()
-print()
-print('eliminar', arbol.delete_node(10))
-arbol.inorden()
